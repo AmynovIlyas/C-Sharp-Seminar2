9f1a5e7630aee02794b6d75096aac02dea1bb16b
Ilyas Aminov <aminov.i.g@yandex.ru>
2023-04-27 22:46:52 +0300

    Understood Return and no-Return Methods

---
+++ CompareFiguresMethodReturn/Program.cs
@@ -0,0 +1,35 @@
+﻿// 
+int rand = new Random().Next(10, 100);
+Console.WriteLine("Случайное число: " + rand);
+int a1 = FindFirstDigitNumber(rand);
+int b1 = FindSecondDigitNumber(rand);
+FindMost(a1, b1);
+
+
+int FindFirstDigitNumber(int r) // Пример возвращаемого метода
+{
+    int result = r / 10;
+    return result;
+}
+
+int FindSecondDigitNumber(int r) // Пример возвращаемого метода
+{
+    int result = r % 10;
+    return result;
+}
+
+void FindMost(int a, int b) // Пример НЕвозвращаемого метода
+{
+    if (a > b)
+    {
+        Console.WriteLine("Наибольшая цифра числа: "+ a);
+    }
+    else if (a == b)
+    {
+        Console.WriteLine("Число состоит из одинаковых цифр");
+    }
+    else
+    {
+        Console.WriteLine("Наибольшая цифра числа: " + b);
+    }
+}
