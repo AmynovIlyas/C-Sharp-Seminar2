dde758fc520e2be496385c19d09b263ef96d5887
Ilyas Aminov <aminov.i.g@yandex.ru>
2023-04-27 21:37:34 +0300

    Solution of the task CompareFutureFigures with Method-no-return

---
+++ CompareFutures10-99/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая выводит случайное (рандомное) число из отрезка [10; 99] и показывает наибольшую цифру числа
+// Например:
+// 78 --> 8
+// 12 --> 3
+// 85 --> 8
+
+void FindMost(int rand)
+{
+    int a = rand / 10;
+    int b = rand % 10;
+    int max = a;
+    if (b > a)
+    {
+        max = b;
+    }
+    Console.WriteLine(max);
+}
+
+int rand = new Random().Next(10, 100);
+Console.WriteLine(rand); 
+FindMost(rand);
+
+
+// Решение без метода:
+// Console.WriteLine("Рандомное число: " + rand);
+// int a = rand / 10;
+// int b = rand % 10;
+// if (a > b)
+// {
+//     Console.WriteLine("Наибольшая цифра числа: " + a);
+// }
+// else if (a == b)
+// {
+//     Console.WriteLine("Введённое число состоит из одинаковых цифр");
+// }
+// else
+// {
+//     Console.WriteLine("Наименьшая цифра числа: " + b);
+// }
