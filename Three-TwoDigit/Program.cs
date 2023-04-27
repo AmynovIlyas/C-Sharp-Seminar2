65b8c8397e8336e33023fcfee78425054ba84c44
Ilyas Aminov <aminov.i.g@yandex.ru>
2023-04-27 23:45:38 +0300

    Independent solution of the task Three-Digit to Two-Digit

---
+++ Three-TwoDigit/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа
+// Например:
+// 456 --> 46 
+// 782 --> 72
+// 918 --> 98
+
+// int rand = new Random().Next(100, 1000);
+// Console.WriteLine("Случайное число: " + rand);
+// DeleteSecondDigit(rand);
+
+// void DeleteSecondDigit(int r)
+// {
+//     int a = rand / 100;
+//     int b = rand % 100 / 10;
+//     int c = rand % 100 % 10;
+//     Console.WriteLine(a * 10 + c);
+// }
+
+int rand = new Random().Next(100, 1000);
+Console.WriteLine("Случайное число: " + rand);
+int a = FindFirstDigit(rand);
+int b = FindSecondDigit(rand);
+int c = FindThirdDigit(rand);
+Console.WriteLine(a * 10 + c);
+
+int FindFirstDigit(int r)
+{
+    int result = rand / 100;
+    return result;
+}
+
+int FindSecondDigit(int r)
+{
+    int result = rand % 100 / 10;
+    return result;
+}
+
+int FindThirdDigit(int r)
+{
+    int result = rand % 100 % 10;
+    return result;
+}
